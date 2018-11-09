1866cf4c7b6281588d96d5d88e64d8c30323b801
Alexey Berkov <alexey.berkley@gmail.com>
2018-11-09 11:19:54 +0700

    initial commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Alexey Berkov on 09/11/2018.
+//  Copyright © 2018 Alexey Berkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
